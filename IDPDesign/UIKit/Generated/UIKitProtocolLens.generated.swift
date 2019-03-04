92455b35c3e52afff35bd1b5ebca7e26c553ecca
Artem Chabannyi <artem.chabanniy@gmail.com>
2019-03-04 17:33:32 +0200

    Removed unused file.

--- IDPDesign/UIKit/Generated/UIKitProtocolLens.generated.swift
+++
@@ -1,5 +0,0 @@
-// Generated using Sourcery 0.9.0 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-
-STATUS:
-Error Domain=NSCocoaErrorDomain Code=260 "The file “UIProtocolLensSpec.stencil” couldn’t be opened because there is no such file." UserInfo={NSFilePath=/Users/trimm/Developer/OSS/IDPDesign/IDPDesign/Templates/UIProtocolLensSpec.stencil, NSUnderlyingError=0x7fbae5f80260 {Error Domain=NSPOSIXErrorDomain Code=2 "No such file or directory"}}
